7a4b7384892ecaee4eff694aefe225f6c79f7260
TestingBread <seedee7755@gmail.com>
2025-07-25 14:43:48 +0700

    update

---
+++ algorithm/CircleArea.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CircleArea {
+    public static void main (String[] args) {
+
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter radius: ");
+    int radius = myObj.nextint();
+
+    System.out.println("radius= "+radius);
+
+    }
+
+
+}
