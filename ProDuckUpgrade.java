1183768a1631a1b3045405181a9da0897c341fda
TestingBread <seedee7755@gmail.com>
2025-09-03 09:33:06 +0700

    produck upgrade

--- ProDuckUpgrade.java
+++ ProDuckUpgrade.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BuyProduct {
+public class ProDuckUpgrade {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         //1
@@ -11,14 +11,22 @@ public class BuyProduct {
         double Price1 = sc.nextDouble();
         sc.nextLine();
 
+        System.out.print("quantity for product1 :");
+        double quantity1 = sc.nextDouble();
+        sc.nextLine();
+
 
         //-------
         //2
         System.out.print("ProDuct #2 Name :");
         String Name2 = sc.nextLine();
-    
+
         System.out.print("ProDuct #2 Price :");
         double Price2 = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.print("quantity for product2 :");
+        double quantity2 = sc.nextDouble();
         sc.nextLine();
 
 
@@ -31,12 +39,20 @@ public class BuyProduct {
         double Price3 = sc.nextDouble();
         sc.nextLine();
 
+        System.out.print("quantity for product3 :");
+        double quantity3 = sc.nextDouble();
+        sc.nextLine();
+
         
         //paid
         System.out.print("Customer Paid :");
         double CPaid = sc.nextDouble();
+
         //mathing
-        double Summary_Price = Price1+Price2+Price3;
+        double item1 = Price1*quantity1;
+        double item2 = Price2*quantity2;
+        double item3 = Price3*quantity3;
+        double Summary_Price = item1+item2+item3;
         double Vat = Summary_Price*0.07;
         double Net = Vat+Summary_Price;
         double change = CPaid-Net;
@@ -45,16 +61,16 @@ public class BuyProduct {
         //output
         System.out.println("-------receipt-------");
 
-        System.out.println(Name1+" = "+Price1+" baht");
-        System.out.println(Name2+" = "+Price2+" baht");
-        System.out.println(Name3+" = "+Price3+" baht");
+        System.out.println(Name1+":"+Price1+" x "+quantity1+" = "+item1);
+        System.out.println(Name2+":"+Price2+" x "+quantity2+" = "+item2);
+        System.out.println(Name3+":"+Price3+" x "+quantity3+" = "+item3);
 
         System.out.println("total price no tax "+Summary_Price);
         System.out.printf("Vat %.2f baht\n",Vat);
         System.out.println("Net price "+Net+" baht");
         System.out.println("Change "+change+" baht");
 
-        //(Raw) Not cooked
+        //🍞Cooked 👍
 
     }
 
