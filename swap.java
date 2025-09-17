7630a597ea92738f08645827ebe9a9062ff6df4f
TestingBread <seedee7755@gmail.com>
2025-09-17 10:11:01 +0700

    added more shop and swaping

--- swap.java
+++ swap.java
@@ -1,3 +1,24 @@
 import java.util.Scanner;
 
-//later
+public class swap {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+    //input
+        System.out.print("Enter num1:");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter num2 :");
+        int num2 = sc.nextInt();
+
+    //swap
+        num1 = num1^num2;
+        num2 = num1^num2;
+        num1 = num1^num2;
+
+    //output
+        System.out.println("num1 is "+num1+"|| num2 is "+num2);
+
+
+    }
+}
